7070e43cc9347240e873f4191062ba578d6db52c
Resa-Obamwonyi <theresaobamwonyi@gmail.com>
2020-07-02 14:40:27 +0100

    set password hashing and user email validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,35 @@
 const router = require('express').Router();
 const User = require('../model/user');
+const bcrypt = require('bcryptjs');
+const { signupValidation, loginValidation } = require('../validation');
 
 
-//Validation
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object({
-
-	name: Joi.string().min(6).required(),
-	email: Joi.string().min(6).required().email(),
-	password: Joi.string().min(6).required()
-});
 
 router.post('/signup', async (req, res) => {
+
 	//lets validate the data before creating a user
-	
-	const {error} = schema.validate(req.body);
+	const {error} = signupValidation(req.body);
 	if(error) return res.status(400).send(error.details[0].message);
 
+	//check if user email already exists
+	const emailExist = await User.findOne({
+		email: req.body.email
+	});
+	if(emailExist) return res.status(400).send('Email already exits');
+	
+
+	//Hash the Password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	//create a new user
 		const user =  new User({
     	name: req.body.name,
     	email: req.body.email,
-    	password: req.body.password
+    	password: hashedPassword
 	});
 	
-//try to submit and catch errors
+   //try to submit and catch errors
     try{
     	const savedUser = await user.save();
     	res.send(savedUser);
---
+++ validation.js
@@ -0,0 +1,34 @@
+//Validation
+const Joi = require('@hapi/joi');
+
+
+//Signup Validation
+const signupValidation = (data) =>{
+    const schema = Joi.object({
+
+        name: 
+            Joi.string().min(6).required(),
+        email: 
+            Joi.string().min(6).required().email(),
+        password: 
+            Joi.string().min(6).required()
+    });
+
+    return schema.validate(data);
+};
+
+//Login Validation
+const loginValidation = (data) =>{
+    const schema = Joi.object({
+
+        email: 
+            Joi.string().min(6).required().email(),
+        password: 
+            Joi.string().min(6).required()
+    });
+
+    return schema.validate(data);
+};
+
+module.exports.signupValidation = signupValidation;
+module.exports.loginValidation = loginValidation;
